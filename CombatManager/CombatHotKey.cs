b075b6d78c7921c5e30aed8b8918aece9517b200
Kyle Olson <kyle@kyleolson.com>
2013-02-16 10:24:36 -0800

    Add missing files for Hotkeys

---
+++ CombatManager/CombatHotKey.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace CombatManager
+{
+    public enum CombatHotKeyType
+    {
+        MeleeAttack,
+        RangedAttack,
+        Save,
+        Skill
+    }
+
+    public class CombatHotKey
+    {
+        Key _Key = Key.A;
+        ModifierKeys _Modifier;
+        CombatHotKeyType _Type;
+        String _Subtype;
+
+        public CombatHotKey() { }
+        public CombatHotKey(CombatHotKey hk)
+        {
+            _Key = hk._Key;
+            _Modifier = hk._Modifier;
+            _Type = hk._Type;
+            _Subtype = hk._Subtype;
+        }
+
+        public Key Key
+        {
+            get { return _Key; }
+            set { _Key = value; }
+        }
+
+        public ModifierKeys Modifier
+        {
+            get { return _Modifier; }
+            set { _Modifier = value; }
+        }
+
+        public CombatHotKeyType Type
+        {
+            get { return _Type; }
+            set { _Type = value; }
+        }
+
+        public String Subtype
+        {
+            get { return _Subtype; }
+            set { _Subtype = value; }
+        }
+
+        public bool AltKey
+        {
+            get { return GetKey(ModifierKeys.Alt); }
+            set
+            {
+                SetKey(ModifierKeys.Alt, value);
+            }
+        }
+        public bool ShiftKey
+        {
+            get { return GetKey(ModifierKeys.Shift); }
+            set
+            {
+                SetKey(ModifierKeys.Shift, value);
+            }
+        }
+        public bool CtrlKey
+        {
+            get { return GetKey(ModifierKeys.Control); }
+            set
+            {
+                SetKey(ModifierKeys.Control, value);
+            }
+        }
+
+        private bool GetKey(ModifierKeys key)
+        {
+            return (_Modifier & key) == key;
+        }
+
+        private void SetKey(ModifierKeys key, bool value)
+        {
+            if (value)
+            {
+                _Modifier |= key;
+            }
+            else
+            {
+                _Modifier &= ~key;
+            }
+        }
+
+    }
+}
---
+++ CombatManager/HotKeysDialog.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace CombatManager
+{
+    /// <summary>
+    /// Interaction logic for HotKeysDialog.xaml
+    /// </summary>
+    public partial class HotKeysDialog : Window
+    {
+		 ObservableCollection<CombatHotKey> _CombatHotKeys;
+		
+        public HotKeysDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void DeleteButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            CombatHotKey hk = (CombatHotKey)((FrameworkElement)sender).DataContext;
+            _CombatHotKeys.Remove(hk);
+        }
+
+        private void CancelButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+			DialogResult = false;
+        	Close();
+        }
+
+        private void OKButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+			DialogResult = true;
+        	Close();
+        }
+
+        private void AddButton_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            _CombatHotKeys.Add(new CombatHotKey());
+        }       
+		
+		public List<CombatHotKey> CombatHotKeys 
+        {
+            get
+            {
+                return new List<CombatHotKey>(_CombatHotKeys);
+            }
+            set
+            {
+                _CombatHotKeys = new ObservableCollection<CombatHotKey>();
+                foreach (CombatHotKey hk in _CombatHotKeys)
+                {
+                    _CombatHotKeys.Add(new CombatHotKey(hk));
+                }
+                KeyListBox.ItemsSource = _CombatHotKeys;
+            }
+        }
+
+		private void CommandComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+		{
+            ComboBox typeCombo = (ComboBox)sender;
+            DependencyObject parent = VisualTreeHelper.GetParent(typeCombo);
+            int count = VisualTreeHelper.GetChildrenCount(parent);
+            ComboBox subtypeCombo = null;
+            for (int i = 0; i < count; i++)
+            {
+                DependencyObject dob = VisualTreeHelper.GetChild(parent, i);
+                if (dob is FrameworkElement)
+                {
+                    FrameworkElement fe = (FrameworkElement)dob;
+                    if (fe.Name == "SubtypeComboBox")
+                    {
+                        subtypeCombo = (ComboBox)fe;
+                        break;
+                    }
+                }
+            
+            }
+
+            subtypeCombo.Items.Clear();
+            switch (typeCombo.SelectedIndex)
+            {
+                case 0:
+                    subtypeCombo.IsEnabled = false;
+                    break;
+                case 1:
+                    subtypeCombo.IsEnabled = false;
+                    break;
+                case 2:
+                    subtypeCombo.IsEnabled = true;
+                    subtypeCombo.Items.Add(new ComboBoxItem() { Content = "Fort" });
+                    subtypeCombo.Items.Add(new ComboBoxItem() { Content = "Ref" });
+                    subtypeCombo.Items.Add(new ComboBoxItem() { Content = "Will" });
+                    subtypeCombo.SelectedIndex = 0;
+                    break;
+                case 3:
+                    subtypeCombo.IsEnabled = true;
+                    foreach (Monster.SkillInfo si in Monster.SkillsDetails.Values)
+                    {
+                        subtypeCombo.Items.Add(new ComboBoxItem() { Content = si.Name, Tag = si });
+                    }
+                    break;
+            }
+		}
+    }
+}
