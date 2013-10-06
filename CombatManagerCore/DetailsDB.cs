43deb991dcdd574ba8ca501bda0043c3a85c1a46
Kyle Olson <kyle@kyleolson.com>
2013-10-06 11:58:58 -0700

    Missing files for last commit

---
+++ CombatManagerCore/DetailsDB.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ScottsUtils;
+
+#if !MONO
+#else
+using Mono.Data.Sqlite;
+#endif
+
+namespace CombatManager
+{
+    static class DetailsDB
+    {
+        #if !MONO
+                private static SQL_Lite sqlDetailsDB;
+        #else
+		
+                private static SqliteConnection detailsDB;
+        #endif
+
+        public static string LoadDetails(string ID, string table, string field)
+        {
+            string details = "";
+            string commandText = "Select " + field + " from " + table +" where ID=?";
+            try
+            {
+#if MONO
+                if (detailsDB == null)
+                {
+#if ANDROID
+                    detailsDB = new SqliteConnection("DbLinqProvider=Sqlite;Data Source=" + DBFullFilename);
+#else
+                    
+                    detailsDB = new SqliteConnection("DbLinqProvider=Sqlite;Data Source=Details.db");
+#endif
+                    detailsDB.Open();
+
+                }
+
+                var cm = detailsDB.CreateCommand();
+                cm.CommandText = commandText;
+                var p = cm.CreateParameter();
+                p.Value = ID;
+                cm.Parameters.Add(p);
+
+                details = (string)cm.ExecuteScalar();
+#else
+                if (sqlDetailsDB == null)
+                {
+                    sqlDetailsDB = new SQL_Lite();
+                    sqlDetailsDB.SkipHeaderRow = true;
+                    sqlDetailsDB.Open("details.db");
+                }
+
+                RowsRet ret = null;
+
+                ret = sqlDetailsDB.ExecuteCommand(commandText, new object[] { ID });
+
+                if (ret == null || ret.Count() == 0)
+                {
+                    return "";
+                }
+
+                details = ret.Rows[0][field];
+
+#endif
+   
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.ToString());
+            }
+
+
+            return details;
+        
+        }
+    }
+}
