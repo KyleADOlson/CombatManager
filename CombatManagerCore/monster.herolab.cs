25378bec520b1d9c4ab0353d0ff2792b4077f0a2
vasseaude <jhunk_bhin@yahoo.com>
2013-12-22 22:04:10 -0600

    Spells are a bitch

---
+++ CombatManagerCore/monster.herolab.cs
@@ -0,0 +1,35 @@
+﻿﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+﻿using System.Diagnostics;
+﻿using System.Linq;
+﻿using System.Runtime.Serialization.Formatters;
+﻿using System.Text;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Globalization;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Xml.Linq;
+using Herolab;
+using Ionic.Zip;
+using System.Threading.Tasks;
+
+
+namespace CombatManager
+{
+    public partial class Monster
+    {
+
+
+
+
+
+
+
+
+    }
+    
+}
